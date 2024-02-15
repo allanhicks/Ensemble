c594ad2746c5d1fccd6bdba9772f1b805079000d
AllanHicks-IPHC <fishmanHicks@gmail.com>
2024-02-15 11:01:05 -0800

    Update createSample.R
    Added in ability to take samples in log space, using an approximation to Sigma in log space.

--- R/createSample.R
+++ R/createSample.R
@@ -1,8 +1,10 @@
 #' Create a multivariate normal sample
 #' Checks for positive definite covariance matrix and makes a correction if not
 #' @param n Sample size
-#' @param means A vector of means
-#' @param Sigma Variance-Covariance matrix
+#' @param means A vector of means (always entered in normal space)
+#' @param Sigma Variance-Covariance matrix (always entered in normal space)
+#' @param logSpace logical: take the natural log of the variable before sampling, then exponentiating back
+#' @param logBiasCorr logical: apply a bias correction if the mean in normal space (rather than the median) is provided for a lognormal sample
 #' @param lower Lower bounds for the parameters (to sample from a truncate multivariate normal)
 #' @param upper Upper bounds for the parameters (to sample from a truncate multivariate normal)
 #' @param truncated Logical. Whether to use the truncated multivariate normal function or not. The truncated multivariate normal function may be slower.
@@ -11,7 +13,7 @@
 #' 
 #' @authors Allan Hicks, Gwladys Lambert
 #' @export
-createSample.fn <- function(n, means, Sigma, lower=rep(-Inf, length = length(mean)), upper=rep( Inf, length = length(mean)), truncated=TRUE, fixPD=T, estAlpha=TRUE) {
+createSample.fn <- function(n, means, Sigma, logSpace=FALSE, logBiasCorr=TRUE, lower=rep(-Inf, length = length(mean)), upper=rep( Inf, length = length(mean)), truncated=TRUE, fixPD=T, estAlpha=TRUE) {
 
 	require(MASS)
 	require(corpcor)
@@ -25,6 +27,31 @@ createSample.fn <- function(n, means, Sigma, lower=rep(-Inf, length = length(mea
 	if(!is.matrix(Sigma)) {
 		Sigma <- as.matrix(Sigma)
 	}
+
+	fullMeans <- means
+	means <- means[keep]
+	fullLower <- lower
+	lower <- lower[keep]
+	lower[is.na(lower)] <- -Inf
+	fullUpper <- upper
+	upper <- upper[keep]
+	upper[is.na(upper)] <- Inf
+
+	if(logSpace) {
+		#this is an approximation
+		cat("WARNING: The Variance/Covariance matrix in log space is an approximation that may not be accurate for large variances\n")
+		ExEy <- matrix(means,nrow=length(means),ncol=length(means)) * t(matrix(means,nrow=length(means),ncol=length(means)))
+
+		if(logBiasCorr) {
+			logVar <- log((sqrt(diag(Sigma))/means)^2 + 1)
+			means <- log(means) - 0.5*logVar
+		}else {
+			means <- log(means)
+		}
+
+		Sigma <- log(1+Sigma/(ExEy))   #Taylor expansion approximation: https://stats.stackexchange.com/questions/46874/covariance-of-transformed-random-variables/46930#46930
+
+	}
 	if(!is.positive.definite(Sigma)) {
 		if(fixPD) {
 				cat("Fixed the covariance matrix to make it positive-definite\n")
@@ -37,21 +64,16 @@ createSample.fn <- function(n, means, Sigma, lower=rep(-Inf, length = length(mea
 				stop("Exiting because can not sample\n")				
 			}
 	}
-	fullMeans <- means
-	means <- means[keep]
-	fullLower <- lower
-	lower <- lower[keep]
-	lower[is.na(lower)] <- -Inf
-	fullUpper <- upper
-	upper <- upper[keep]
-	upper[is.na(upper)] <- Inf
-
 	if(truncated) {
 		samp <- rtmvnorm.rejection(n, means, Sigma, lower=lower, upper=upper,estimatedAlpha=estAlpha)
 	} else {
 		samp <- mvrnorm(n, means, Sigma)
 	}
 
+	if(logSpace) {
+		#transform back to normal space
+		samp <- exp(samp)
+	}
 	#now, piece it back together because fixed values were removed.
 	fullSamp <- matrix(NA,nrow=nrow(samp),ncol=ncol(fullSigma),dimnames=list(NULL,colnames(fullSigma)))
 	fullSamp[,keep] <- samp
